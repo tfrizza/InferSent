a28bd7770cc04382239e2f9c71ed4742d272691d
Tristan <tfrizza@gmail.com>
2019-05-24 11:28:19 +1000

    made into package

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup
+
+NAME = "InferSent"
+VERSION = "0.0.1nl"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="My custom InterSent",
+    author_email="",
+    url="",
+    keywords=[],
+) 
